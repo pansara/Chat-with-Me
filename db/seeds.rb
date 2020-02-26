5d7725017ecbfc33eda2584abd0aa3e4dea2ef3c
pansara <shivanipansara222@gmail.com>
2020-02-26 14:12:24 -0500

    Build User and Message resource

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Shivani", password: "password")
+User.create(username: "Pansara", password: "password")
+User.create(username: "SPansara", password: "password")
+User.create(username: "John", password: "password")
+User.create(username: "Doe", password: "password")
