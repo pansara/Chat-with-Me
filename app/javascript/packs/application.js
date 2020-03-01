0e67d784700dae39df3b5d30e6ef711e96d4650b
pansara <shivanipansara222@gmail.com>
2020-03-01 13:29:35 -0500

    Modify UI design

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,3 +17,9 @@ import "bootstrap"
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+scroll_bottom = function() {
+    if ($('#messages').length > 0) {
+        $('#messages').scrollTop($('#messages')[0].scrollHeight);
+    }
+}
